aaf284290e9bad5a6b4c4d9dfe745782eda6f305
Kwanchanok231058 <265345221002-st@rmutsb.ac.th>
2023-04-28 11:28:21 +0700

    first commit 265345221002

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react'
-import {MyAbout,MyImage,MyData} from './func-mydata'
-import {Navbar,Header,Footer}  from './func-components'
-import Calendar from './class-components'
-import MsgBox from './func-props'
-
-
+//import {MyAbout,MyImage,MyData} from './func-mydata'
+//import {Navbar,Header,Footer}  from './func-components'
+//import Calendar from './class-components'
+//import MsgBox from './func-props'
+//import MsgBox2 from './class-props'
+import List from './list-comp'
 
 function App() {
+  return (<List/>)
 
-return [<Navbar />,<Calendar />,<Header />,<Footer/>]
+//return [<Navbar />,<Calendar />,<Header />,<Footer/>]
 
   /*return(
-      <MsgBox 
+      <MsgBox2
       text="Useing props in function component"
-      color="red"
+      color="green"
       bgColor="#ccc"
       fontSize="16px"
       border="dotted 1px black"
+      />
   )*/
 }
-
 export default App;
